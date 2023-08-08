2191f589b277a35083ef52b832d1f40d08158643
Ryan <rymoran34@gmail.com>
2023-08-07 19:39:57 -0500

    Added exception class

--- src/main/java/com/shopify/monitor/shopifymonitor/exceptions/ShopifyException.java
+++ src/main/java/com/shopify/monitor/shopifymonitor/exceptions/ShopifyException.java
@@ -1,4 +1,13 @@
 package com.shopify.monitor.shopifymonitor.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@ToString
 public class ShopifyException {
+    String status;
+    String message;
 }
